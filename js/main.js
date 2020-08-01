87ed7d39e879d3a142b066ae552c438da171f74f
Brandon Mushore <you@example.com>
2020-08-01 18:54:30 +0200

    careers

--- js/main.js
+++ js/main.js
@@ -3,3 +3,31 @@ $(".myBtn").click(function() {
         scrollTop: $(".billboard").offset().top},
         'slow');
 });
+
+
+var owl= $('.owl-carousel');
+
+var carouselOption={
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    nav:false,
+    autoplayTimeout:400000,
+    navigation:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:1
+        },
+        1440:{
+            items:1
+        },
+       
+    }
+
+}
+owl.owlCarousel(carouselOption);
